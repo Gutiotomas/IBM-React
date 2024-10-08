5c6e7aecd06eea1a4de6524bc9bb36d95d0c70ba
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-08 18:33:16 -0500

    Remove unused CSS and JavaScript files

--- src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer>
-            <p>&copy; 2024 Event Planner Organization. All Rights Reserved.</p>
-        </footer>
-    );
-}
-
-export default Footer;
