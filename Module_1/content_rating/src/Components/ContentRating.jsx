4e185a0dfc2cbd81aa6f0bfd51b6a5bd3307e01d
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-08 12:34:03 -0500

    Refactor ContentRating component to add total ratings and update button event handlers

--- Module_1/content_rating/src/Components/ContentRating.jsx
+++ Module_1/content_rating/src/Components/ContentRating.jsx
@@ -1,5 +1,5 @@
-import { Component } from "react";
-import "./ContentRating.css";
+import { Component } from 'react';
+import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
@@ -7,37 +7,38 @@ class ContentRating extends Component {
     this.state = {
       likes: 0,
       dislikes: 0,
+      totalRatings: 0,
+      handleLike: () => {
+        this.setState((prevState) => ({
+          likes: prevState.likes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      },
+      handleDislike: () => {
+        this.setState((prevState) => ({
+          dislikes: prevState.dislikes + 1,
+          totalRatings: prevState.totalRatings + 1
+        }));
+      }
     };
   }
-
-  // Define the event handlers as class methods
-  handleLike = () => {
-    this.setState((prevState) => ({
-      likes: prevState.likes + 1,
-    }));
-  };
-
-  handleDislike = () => {
-    this.setState((prevState) => ({
-      dislikes: prevState.dislikes + 1,
-    }));
-  };
-
   render() {
     return (
-      <div className="content-rating">
-        <p>React is a JavaScript library for building user interfaces.</p>
-        <div className="rating-buttons">
-          <button className="like-button" onClick={this.handleLike}>
-            Like ({this.state.likes})
-          </button>
-          <button className="dislike-button" onClick={this.handleDislike}>
-            Dislike ({this.state.dislikes})
-          </button>
+      <>
+        <div className='content-rating'>
+          <p>React is the best</p>
+          <div className='rating-buttons'>
+            <button className="like-button" onClick={this.state.handleLike}>
+              Like ({this.state.likes})
+            </button>
+            <button className="dislike-button" onClick={this.state.handleDislike}>
+              Dislike ({this.state.dislikes})
+            </button>
+          </div>
+          <p>Total Ratings: {this.state.totalRatings}</p>
         </div>
-      </div>
+      </>
     );
   }
 }
-
 export default ContentRating;
