8db8c3a9fd346fd8c30a1c64a903c9ed3ef43e4d
Tomás Gutiérrez Orrego <111087086+Gutiotomas@users.noreply.github.com>
2024-10-15 18:16:18 -0500

    Refactor Vite configuration and main entry point for React app

---
+++ Module_4/conference_event_planner/src/avSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const avSlice = createSlice({
+  name: "av",
+  initialState: [],
+
+  reducers: {
+    incrementAvQuantity: (state, action) => {},
+    decrementAvQuantity: (state, action) => {},
+  },
+});
+
+export const { incrementAvQuantity, decrementAvQuantity } = avSlice.actions;
+
+export default avSlice.reducer;
